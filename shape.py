f3b63999f853f6a1b21b671a8fce438c57d97110
pratikbhojane2828-commits <pratikbhojane2828@gmail.com>
2026-04-11 09:27:43 +0530

    Implement area calculation for circle and rectangle
    Added user input functionality to calculate area for circle and rectangle.

--- shape.py
+++ shape.py
@@ -8,3 +8,20 @@ return area
 def triangle_area(base, height):
 area = 0.5 * base * height
 return area
+
+
+import shape
+print("Choose a shape to calculate area")
+print("1.area_of_circle")
+print("2.area_of_reactagle")
+
+choice=int(input("enter your choice="))
+if choice==1:
+    radius=float(input("enter the radius="))
+    result=shape.area_of_circle(radius)
+    print("area of circle is=",result)
+elif choice==2:
+    height=int(input("enter the height of reactangle="))
+    width=int(input("enter the width of reatangle="))
+    result=shape.area_of_rectangle(height,width)
+    print("area_of rectangle=",result)
